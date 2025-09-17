7b8e5c78b8aa9a4f49a781a8904aec27f5bec7ac
Greg <gregmichaeldavies@gmail.com>
2025-09-17 13:04:45 +0100

    tech: Fixed some barreling.

--- every-app/src/app/app.tsx
+++ every-app/src/app/app.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
-
 import { Route, Routes, Link } from 'react-router-dom';
+import {
+  AppHeader,
+  AppThemeProvider,
+} from 'ui-components';
 
 const StyledApp = styled.div`
   // Your style here
@@ -9,46 +11,48 @@ const StyledApp = styled.div`
 
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="every-app" />
+    <AppThemeProvider>
+      <StyledApp>
+        <AppHeader title='Gregory, Michael & Davies' />
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </StyledApp>
+        {/* START: routes */}
+        {/* These routes and navigation have been generated for you */}
+        {/* Feel free to move and update them to fit your needs */}
+        <br />
+        <hr />
+        <br />
+        <div role="navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page-2">Page 2</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                This is the generated root route.{' '}
+                <Link to="/page-2">Click here for page 2.</Link>
+              </div>
+            }
+          />
+          <Route
+            path="/page-2"
+            element={
+              <div>
+                <Link to="/">Click here to go back to root page.</Link>
+              </div>
+            }
+          />
+        </Routes>
+        {/* END: routes */}
+      </StyledApp>
+    </AppThemeProvider>
   );
 }
 
--- ui-components/src/index.ts
+++ ui-components/src/index.ts
@@ -1,2 +1,5 @@
+export * from './app-header';
+export * from './blog';
 export * from './components';
 export * from './styles';
+export * from './theme';
