f003a09c3a8dc3a11a4fa38778d169f3bee4c42c
Greg <gregmichaeldavies@gmail.com>
2025-09-30 22:50:57 +0100

    feat: Exported typography.

--- ui-components/src/typography/components/Typography.stories.tsx
+++ ui-components/src/typography/components/Typography.stories.tsx
@@ -1,4 +1,4 @@
-import { Typography } from '@mui/material';
+import { Typography } from './Typography';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof Typography> = {
