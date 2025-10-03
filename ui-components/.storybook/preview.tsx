49be64a161e9a9a1cf16eefaf232b295670c164c
Greg <gregmichaeldavies@gmail.com>
2025-10-03 20:13:34 +0100

    tech: Updated preview to test breakpoints better.

--- ui-components/.storybook/preview.tsx
+++ ui-components/.storybook/preview.tsx
@@ -1,10 +1,28 @@
 import { Preview } from '@storybook/react';
-import { AppThemeProvider, ThemeSwitcher } from '../src/theme';
+import {
+  AppThemeProvider,
+  ThemeSwitcher,
+} from '../src/theme';
 import { ErrorBoundary } from 'react-error-boundary';
+
+import { MINIMAL_VIEWPORTS } from 'storybook/viewport';
 
 const preview: Preview = {
   parameters: {
     layout: 'centered',
+    viewport: {
+      options: {
+        ...MINIMAL_VIEWPORTS,
+        tablet: {
+          name: 'Tablet',
+          styles: {
+            width: '1024px',
+            height: '1366px',
+          },
+          type: 'tablet',
+        },
+      },
+    }
   },
   decorators: [
     (Story) => (
