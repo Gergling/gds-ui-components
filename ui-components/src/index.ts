8777c1ffdc14052d03bd6ac284bf7d5cdf93247b
Greg <gregmichaeldavies@gmail.com>
2025-10-04 13:04:04 +0100

    tech: Barreled page container.

--- ui-components/src/index.ts
+++ ui-components/src/index.ts
@@ -2,6 +2,7 @@ export * from './app-header';
 export * from './blog';
 export * from './components';
 export * from './metric-chip';
+export * from './page-container';
 export * from './styles';
 export * from './theme';
 export * from './typography';
