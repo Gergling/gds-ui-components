ce9e4228c88850a5a9d2ce8029a79d9a8e8f7ba3
Greg <gregmichaeldavies@gmail.com>
2025-11-02 09:19:48 +0000

    feat: Added some base colours for possible GDS default.

--- ui-components/src/styles/base-colors.ts
+++ ui-components/src/styles/base-colors.ts
@@ -3,6 +3,12 @@
 
 export const baseColors = {
   daisybush006: '#59288a',
+  darkburgundy004a: '#730d15',
+  darkburgundy004: '#770F05',
+  darktan003: '#661010',
   earlydawn094: '#fcf9e8',
+  firebrick005: '#800517',
+  moccaccino: '#6E1D14',
+  persianplum: '#701C1C',
   yellowmetal012: '#6b612e',
 };
